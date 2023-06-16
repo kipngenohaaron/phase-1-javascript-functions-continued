f0f8c70066b756aa485889e3f7577fb7886681ed
kipngenohaaron <kipngenohaaron@gmail.com>
2023-06-16 16:26:16 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+ 
+  const result = (function (baseNumber) {
+    return baseNumber + 3;
+  })(2);
+  
+  console.log(result); 
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+
+  
